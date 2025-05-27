e81715d5f3a04dd36e5450ebc5db4f9b694d63b0
Tạ Phong <phong.th210671@sis.hust.edu.vn>
2025-05-27 22:47:06 +0700

    Add files via upload
    Signed-off-by: Tạ Phong <phong.th210671@sis.hust.edu.vn>

---
+++ employee/form.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset = "utf-8">
+	<meta name = "viewport" content = "width = device - width, initial - scale = 1">
+	<title>Software Dev</title>
+
+<link rel = "stylesheet" type = "text/css" href = "style.css">
+
+</head>
+<body>
+	<div class="center">
+		<h1>Employee Data Entry Software</h1>
+		<div class="form">
+			<input type="text" class="textfield" placeholder="Search ID">
+			<input type="text" class="textfield" placeholder="Employee name">
+
+			<select>
+				<option>Select department</option>
+				<option>Executive</option>
+				<option>Human Resources</option>
+				<option>Accounting</option>
+				<option>R&D</option>
+				<option>IT</option>
+				<option>Sales</option>
+				<option>Marketing</option>
+				<option>Customer Support</option>
+			</select>
+
+			<input type="text" class="textfield" placeholder="Email address">
+
+			<select>
+				<option>Gender</option>
+				<option>Male</option>
+				<option>Female</option>
+				<option>Other</option>
+			</select>
+			
+			<textarea placeholder = "Address"></textarea>
+
+			<input type = "submit" value="Search" name="" class="btn">
+		</div>
+	</div>
+</body>
+</html>
