968c60d8208ade44ecaf3ba1c404b55ff8264d14
Tạ Phong <phong.th210671@sis.hust.edu.vn>
2025-05-30 19:10:25 +0700

    Update form.php
    Signed-off-by: Tạ Phong <phong.th210671@sis.hust.edu.vn>

--- employee/form.php
+++ employee/form.php
@@ -1,3 +1,9 @@
+<?php
+
+include("connection.php");
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,13 +16,15 @@
 </head>
 <body>
 	<div class="center">
+
+		<form action ="#" method="POST"> 
 		<h1>Employee Data Entry Software</h1>
 		<div class="form">
-			<input type="text" class="textfield" placeholder=" Search ID">
-			<input type="text" class="textfield" placeholder=" Employee name">
+			<input type="text" name="id" class="textfield" placeholder=" ID">
+			<input type="text" name="name" class="textfield" placeholder=" Employee Name">
 
-			<select class="textfield">
-				<option>Select department</option>
+			<select class="textfield" name="department">
+				<option>Department</option>
 				<option>Executive</option>
 				<option>Human Resources</option>
 				<option>Accounting</option>
@@ -27,19 +35,49 @@
 				<option>Customer Support</option>
 			</select>
 
-			<input type="text" class="textfield" placeholder=" Email address">
+			<input type="text" name="email" class="textfield" placeholder=" Email">
 
-			<select class="textfield">
+			<select class="textfield" name="gender">
 				<option>Gender</option>
 				<option>Male</option>
 				<option>Female</option>
 				<option>Other</option>
 			</select>
 			
-			<textarea placeholder="Address"></textarea>
+			<textarea placeholder="Address" name="address"></textarea>
 
-			<input type = "submit" value="Search" name="" class="btn">
+			<input type = "submit" name="save" value="SAVE" name="" class="btn">
+
 		</div>
+	</form>
 	</div>
 </body>
 </html>
+
+<?php
+if(isset($_POST['save']))
+{
+	$id 		= $_POST['id'];
+	$name 		= $_POST['name'];
+	$department = $_POST['department'];
+	$email 		= $_POST['email'];
+	$gender 	= $_POST['gender'];
+	$address 	= $_POST['address'];
+
+	$query = "INSERT INTO employees (emp_id, emp_name, emp_department, emp_email, emp_gender, emp_address) VALUES('$id','$name','$department','$email','$gender','$address')";
+
+	$data = mysqli_query($conn,$query);
+	//cmd to insert info from the register table into the database
+
+	if($data)
+	{
+		echo "Data saved successfully";
+	}
+	else
+	{
+		echo "Failed to save data: " . mysqli_error($data);
+	}
+}
+?>
+
+
